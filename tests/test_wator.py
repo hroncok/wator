8f168e79d3ac9bad3bd56640142052b608ef0291
Miro Hrončok <miro@hroncok.cz>
2017-07-27 10:21:46 +0200

    Initial commit

---
+++ tests/test_wator.py
@@ -0,0 +1,101 @@
+import numpy
+import pytest
+
+from wator import WaTor
+
+
+def test_init_creatures():
+    creatures = numpy.zeros((8, 8))
+    creatures[2, 4] = 3
+    creatures[1, :] = -5
+    wator = WaTor(creatures=creatures)
+    print(wator.creatures)
+    assert (creatures == wator.creatures).all()
+    assert wator.count_fish() == 1
+    assert wator.count_sharks() == 8
+
+
+def test_init_shape():
+    shape = (16, 16)
+    wator = WaTor(shape=shape, nfish=16, nsharks=4)
+    print(wator.creatures)
+    assert wator.creatures.shape == shape
+    assert wator.count_fish() == 16
+    assert wator.count_sharks() == 4
+    assert ((wator.creatures >= -10) & (wator.creatures <= 5)).all()
+
+
+def test_init_shape_custom_age():
+    shape = (16, 16)
+    age_fish = 2
+    age_shark = 20
+    wator = WaTor(shape=shape, nfish=16, nsharks=4,
+                  age_fish=age_fish,
+                  age_shark=age_shark)
+    print(wator.creatures)
+    assert wator.creatures.shape == shape
+    assert wator.count_fish() == 16
+    assert wator.count_sharks() == 4
+    assert ((wator.creatures >= -age_shark) &
+            (wator.creatures <= age_fish)).all()
+
+
+def test_energy_initial_default():
+    shape = (8, 8)
+    creatures = numpy.zeros(shape)
+    creatures[1, :] = -5
+    wator = WaTor(creatures=creatures)
+    print(wator.creatures)
+    print(wator.energies)
+    assert wator.energies.shape == shape
+    assert (wator.energies[1, :] == 5).all()
+
+
+def test_energy_initial_custom():
+    shape = (8, 8)
+    creatures = numpy.zeros(shape)
+    creatures[1, :] = -5
+    wator = WaTor(creatures=creatures, energy_initial=12)
+    print(wator.creatures)
+    print(wator.energies)
+    assert wator.energies.shape == shape
+    assert (wator.energies[1, :] == 12).all()
+
+
+def test_energies():
+    shape = (8, 8)
+    creatures = numpy.zeros(shape)
+    energies = numpy.zeros(shape)
+    creatures[1, :] = -5
+    for i in range(8):
+        energies[1, i] = i
+    wator = WaTor(creatures=creatures, energies=energies)
+    print(wator.creatures)
+    print(wator.energies)
+    assert (wator.energies[1, :] == energies[1, :]).all()
+
+
+def test_nonsense():
+    creatures = numpy.zeros((8, 8))
+
+    with pytest.raises(ValueError):
+        WaTor(creatures=creatures, nfish=20)
+
+    with pytest.raises(ValueError):
+        WaTor(creatures=creatures, nsharks=20)
+
+    with pytest.raises(ValueError):
+        WaTor(creatures=creatures, shape=(8, 8))
+
+    with pytest.raises(ValueError):
+        WaTor(shape=(8, 8), nsharks=20)
+
+    with pytest.raises(ValueError):
+        WaTor(creatures=creatures, energies=numpy.zeros((6, 6)))
+
+    with pytest.raises(ValueError):
+        WaTor(creatures=creatures, energies=numpy.zeros((8, 8)),
+              energy_initial=8)
+
+    with pytest.raises(TypeError):
+        WaTor(shape=(8, 8), nsharks=20, nfish='nope')
---
+++ wator.py
@@ -0,0 +1,74 @@
+import numpy
+
+
+ENERGY_INITIAL = 5
+ENERGY_EAT = 1
+AGE_FISH = 5
+AGE_SHARK = 10
+
+
+class WaTor:
+    def __init__(self, *, creatures=None, shape=None,
+                 nfish=None, nsharks=None,
+                 age_fish=AGE_FISH, age_shark=AGE_SHARK,
+                 energy_initial=None, energies=None, energy_eat=ENERGY_EAT):
+        self.age_fish, self.age_shark = int(age_fish), int(age_shark)
+
+        self.creatures = self.create_creatures(creatures, shape,
+                                               nfish, nsharks)
+
+        self.energies = self.create_energies(energy_initial, energies,
+                                             self.creatures.shape)
+
+        self.energy_eat = int(energy_eat)
+
+    def create_creatures(self, creatures, shape, nfish, nsharks):
+        if isinstance(creatures, numpy.ndarray):
+            if shape is not None or nfish is not None or nsharks is not None:
+                raise ValueError('cannot use creatures and '
+                                 'shape/nfish/nsharks together')
+            return creatures
+
+        if shape is None or nfish is None or nsharks is None:
+            raise ValueError('when using shape/nfish/nsharks, '
+                             'you need to define all')
+        return self.random_population(shape, nfish, nsharks)
+
+    @classmethod
+    def random_cell(cls, shape):
+        return (numpy.random.randint(shape[0]),
+                numpy.random.randint(shape[1]))
+
+    def random_population(self, shape, nfish, nsharks):
+        creatures = numpy.zeros(shape, dtype=numpy.int8)
+        if (nfish + nsharks) > creatures.size:
+            raise ValueError('too many creatures for small shape')
+        while nfish > 0 or nsharks > 0:
+            cell = self.random_cell(shape)
+            while creatures[cell] != 0:
+                cell = self.random_cell(shape)
+            if nfish > 0:
+                creatures[cell] = numpy.random.randint(1, self.age_fish + 1)
+                nfish -= 1
+            else:
+                creatures[cell] = -numpy.random.randint(1, self.age_shark + 1)
+                nsharks -= 1
+        return creatures
+
+    @classmethod
+    def create_energies(cls, energy_initial, energies, shape):
+        if energy_initial is None:
+            if energies is None:
+                return numpy.full(shape, ENERGY_INITIAL)
+            if energies.shape != shape:
+                raise ValueError('energies array has incorrect shape')
+            return energies
+        if energies is not None:
+            raise ValueError('cannot use energies and energy_initial together')
+        return numpy.full(shape, int(energy_initial))
+
+    def count_fish(self):
+        return (self.creatures > 0).sum()
+
+    def count_sharks(self):
+        return (self.creatures < 0).sum()
